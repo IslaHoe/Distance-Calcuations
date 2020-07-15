eacc2ca0666a8ba6812c802413a5f7467d95d937
Isla Hoe <ihoe@zendesk.com>
2020-07-15 10:57:42 +0100

    Update build path

--- distanceChallange/customers.sh
+++ distanceChallange/customers.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-rm customers.txt
+rm customers.txt 
 curl https://s3.amazonaws.com/intercom-take-home-test/customers.txt >> customers.txt
 
-go build
-./codingChallange
+go build 
+./distanceChallange
 open output.txt
